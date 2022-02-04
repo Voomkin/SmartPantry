fae73f46a681ac150f4db4983dc96c87986a21b1
Ryan Mraz <rmm0049@mix.wvu.edu>
2022-02-04 13:06:59 -0500

    graphql and storage inital setup

---
+++ src/graphql/queries.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getItems = /* GraphQL */ `
+  query GetItems {
+    getItems {
+      id
+      name
+      description
+      weight
+      location
+    }
+  }
+`;
