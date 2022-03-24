9619f32039b181ac4b585d21a787c31ca05ad2f9
Kollin Labowski <kkl0009@mix.wvu.edu>
2022-03-23 21:14:04 -0400

    Formatted Notifications page, some initial functions for saving user input

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -1,3 +1,8 @@
+/*
+Note: As of 3/23/2022, initial layout of page complete. In progress: Saving the notification preference of the user based on the scroll input.
+TODO: Send push notifications regularly, and at a frequency based on the user's preference (probably from hourly to never)
+*/
+
 import React, {useState, useRef} from 'react';
 import {
   SafeAreaView,
@@ -6,7 +11,31 @@ import {
   View,
   Animated,
   PanResponder,
+  Button,
+  Alert,
 } from 'react-native';
+import { Notifications } from 'expo';
+// import * as Permissions from 'expo-permissions';
+
+
+
+let notification_preference = 5;
+
+const UpdatePreference = ( new_preference ) => {
+    // alert(new_preference);
+    notification_preference = new_preference;
+}
+
+const GetPreference = () => {
+    return notification_preference;
+}
+
+const TestFunction = ( test_value ) => {
+    if(test_value > 10)
+        alert("GREATER");
+    else
+        alert("LESS");
+};
 
 const NotificationsScreen = () => {
   const [sliderDimensions, setSliderDimensions] = useState({
@@ -36,18 +65,24 @@ const NotificationsScreen = () => {
     },
   });
 
+  let outer_y = 200;
+
   return (
     <SafeAreaView>
       <Text style={styles.title}>How often would you like to receive notifications?</Text>
       <View
         style={styles.slider}
         onLayout={(evt) => {
-          const {height, y} = evt.nativeEvent.layout;
+          let {height, y} = evt.nativeEvent.layout;
+          outer_y = y;
+
           setSliderDimensions({
             height: height,
             top: y,
             bottom: y + height,
           });
+
+        //   UpdatePreference(y);
         }}>
         <View style={styles.rail}>
           <Animated.View style={[styles.railFill, {height: railFillAnim}]}>
@@ -72,8 +107,19 @@ const NotificationsScreen = () => {
               transform: [{translateY: stepperAnim}],
             },
           ]}
+
+        //   { ...outer_y += 10 }
         />
       </View>
+      <Button
+        onPress={() => {
+            UpdatePreference(outer_y);
+            Alert.alert("New preference: " + notification_preference);
+        }}
+        title="Confirm"
+        color="blue"
+        accessibilityLabel="Click here to confirm your notification preference"
+      />
     </SafeAreaView>
   );
 };
@@ -83,30 +129,32 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     marginVertical: 50,
+    marginBottom: 100,
   },
   slider: {
+    marginVertical: 50,
     width: 50,
-    height: '120%',
+    height: '40%',
     marginLeft: 'auto',
     marginRight: 'auto',
     position: 'relative',
-    marginBottom: 50,
+    marginBottom: 0,
   },
   rail: {
     width: 20,
-    height: '100%',
+    height: '50%',
     marginLeft: 'auto',
     marginRight: 'auto',
     backgroundColor: '#DBDBDB',
   },
   stepper: {
     width: '100%',
-    height: 5,
-    backgroundColor: 'black',
+    height: 15,
+    backgroundColor: 'gray',
   },
   railFill: {
     width: '100%',
-    backgroundColor: '#CBAA71',
+    backgroundColor: 'lightgreen',
     position: 'absolute',
     bottom: 0,
   },
@@ -117,5 +165,12 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+// const useNotifications = (notificationListener) => {
+//     useEffect(() => {
+//       registerForPushNotifications();
+  
+//       if (notificationListener) Notifications.addListener(notificationListener);
+//     }, []);
 
 export default NotificationsScreen;
