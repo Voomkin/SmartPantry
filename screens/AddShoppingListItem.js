9d25b5d97fcdc93468e5acd7fdbcbe3eca7df982
Kollin Labowski <kkl0009@mix.wvu.edu>
2022-03-26 21:05:53 -0400

    Added temporary (?) fix to shopping list bug

--- screens/AddShoppingListItem.js
+++ screens/AddShoppingListItem.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Alert } from "react-native";
 import { Button, Input } from "react-native-elements";
 import {Auth, API, graphqlOperation } from 'aws-amplify';
 import { createItem } from "../mutations";
-
-
 
 const AddShoppingListItemScreen = ({ navigation }) => {
   
@@ -24,7 +22,8 @@ const addShoppingListItem = async () => {
   const inputItem = await API.graphql(
     graphqlOperation(createItem, { input: itemInput })
   );
-  navigation.navigate("ShoppingStack");
+//   navigation.navigate("HomeStack"); // NOTE: THIS IS WHERE CURRENT ERROR IS OCCURING, FIX THIS
+Alert.alert("Shopping List", "Added " + nameText + " to your Shopping List"); // Temporary fix, unless we prefer this method
 };
   
   return (
