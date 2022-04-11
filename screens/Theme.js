7e869aff4b06e26559d5239bbfd778f44aabe1b0
Voomkin <malcombjonathan@gmail.com>
2022-04-11 17:29:35 -0400

    changed styling of Cards, added Searchbar from react-native-paper

--- screens/Theme.js
+++ screens/Theme.js
@@ -33,10 +33,22 @@ const customTheme = extendTheme({
   
     // Make sure values below matches any of the keys in `fontConfig`
     fonts: {
-      heading: 'Lato',
+      heading: 'Lato-Bold',
       body: 'Lato-Black',
-      mono: 'Lato',
+      mono: 'Lato-Thin',
     },
+
+    colors: {
+      green: {
+        500: '#81B29A'
+      },
+      cream: {
+        500: '#F4F1DE'
+      },
+      orange: {
+        500: '#E07A5F'
+      }
+    }
 });
 
 export default customTheme
